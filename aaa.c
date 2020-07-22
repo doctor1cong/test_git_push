1dabe095c549a93acc4e8fa0a6a8a9c9afe302ef
doctor1cong <doctor1cong@gmail.com>
2020-07-22 22:22:11 +0800

    changed aaa.c

--- aaa.c
+++ aaa.c
@@ -1,1 +1,19 @@
-wdnmd
+#include<stdio.h>
+
+int main(int argc,char *argv[])
+{
+	int a;
+	if(*argv[1]=printf)
+	{
+		printf("aaaa");
+	}
+	else if(*argv[1]="1")
+	{
+		printf ("1");
+	}
+	else
+	{
+		printf("null");
+	}
+
+}
